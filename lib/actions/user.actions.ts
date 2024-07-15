566f71b47ca2293bc74650b284834bd1c980de6f
Purnima <purnimavats6789@gmail.com>
2024-07-15 17:45:59 +0530

    implements complete auth UI

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,17 @@
+"use server"
+
+export const signIn = async () => {
+    try {
+        // Mutation / Database / Make fetch
+    } catch (error) {
+        console.log("Error", error)
+    }
+}
+
+export const signUp = async (userData: SignUpParams) => {
+    try {
+        // Create a user account
+    } catch (error) {
+        console.log("Error", error)
+    }
+}
