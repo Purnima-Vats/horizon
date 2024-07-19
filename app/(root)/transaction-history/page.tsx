209fcee63a1cfde13e7c164fd297aaf17ef53017
Purnima <purnimavats6789@gmail.com>
2024-07-19 18:31:29 +0530

    finalizing the app with overflow on home page

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -1,56 +1,82 @@
-import HeaderBox from '@/components/HeaderBox'
-import TransactionTable from '@/components/TransactionTable';
-import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
-import { getLoggedInUser } from '@/lib/actions/user.actions';
-import { formatAmount } from '@/lib/utils';
+import HeaderBox from "@/components/HeaderBox";
+import { Pagination } from "@/components/Pagination";
+import TransactionsTable from "@/components/TransactionTable";
+import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
+import { formatAmount } from "@/lib/utils";
+import React from "react";
 
-const TransactionHistory = async ({ searchParams: { id, page}}: SearchParamProps) => {
-    const currentPage = Number(page as string) || 1;    
+const TransactionHistory = async ({
+    searchParams: { id, page },
+}: SearchParamProps) => {
+    const currentPage = Number(page as string) || 1;
     const loggedIn = await getLoggedInUser();
-    // if(!loggedIn) return <div>Please login to access home page</div>
-    const accounts = await getAccounts({userId: loggedIn.$id!});
-    // if(!accounts) return <div>No accounts found</div>
+    const accounts = await getAccounts({
+        userId: loggedIn.$id,
+    });
+
+    if (!accounts) return;
+
     const accountsData = accounts?.data;
     const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
-    const account = await getAccount({appwriteItemId})
-    
+
+    const account = await getAccount({ appwriteItemId });
+
+    const rowsPerPage = 10;
+    const totalPages = Math.ceil(account?.transactions.length / rowsPerPage);
+
+    const indexOfLastTransaction = currentPage * rowsPerPage;
+    const indexOfFirstTransaction = indexOfLastTransaction - rowsPerPage;
+
+    const currentTransactions = account?.transactions.slice(
+        indexOfFirstTransaction,
+        indexOfLastTransaction
+    );
     return (
-        <div className='transactions'>
-            <div className='transactions-header'>
+        <div className="transactions">
+            <div className="transactions-header">
                 <HeaderBox
                     title="Transaction History"
                     subtext="See your bank details and transactions."
                 />
             </div>
 
-            <div className='space-y-6'>
-                <div className='transactions-account'>
-                    <div className='flex flex-col gap-2'>
-                        <h2 className='text-18 font-bold text-white'>{account?.data.name}</h2>
-                        <p className='text-14 text-blue-25'>
+            <div className="space-y-6">
+                <div className="transactions-account">
+                    <div className="flex flex-col gap-2">
+                        <h2 className="text-18 font-bold text-white">
+                            {account?.data.name}
+                        </h2>
+                        <p className="text-14 text-blue-25">
                             {account?.data.officialName}
                         </p>
-                        <p className='text-14 font-semibold tracking-[1.1px] text-white'>
-                        ●●●● ●●●● ●●●● <span className='text-16'>{account?.data.mask || 1234}</span>
+                        <p className="text-14 font-semibold tracking-[1.1px] text-white">
+                            ●●●● ●●●● ●●●● {account?.data.mask}
                         </p>
                     </div>
 
-                    <div className='transactions-account-balance'>
-                        <p className='text-14'>
-                            Current Balance
-                        </p>
-                        <p className='text-24 text-center font-bold'>
+                    <div className="transactions-account-balance">
+                        <p className="text-14">Current balance</p>
+                        <p className="text-24 text-center font-bold">
                             {formatAmount(account?.data.currentBalance)}
                         </p>
                     </div>
                 </div>
 
-                <section className='flex w-full flex-col gap-6'>
-                    <TransactionTable transactions={account?.transactions}/>
+                <section className="flex w-full flex-col gap-6">
+                    <TransactionsTable transactions={currentTransactions} />
+                    {totalPages > 1 && (
+                        <div className="my-4 w-full">
+                            <Pagination
+                                totalPages={totalPages}
+                                page={currentPage}
+                            />
+                        </div>
+                    )}
                 </section>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default TransactionHistory
+export default TransactionHistory;
--- components/TransactionTable.tsx
+++ components/TransactionTable.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {
     Table,
     TableBody,
@@ -8,6 +7,7 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table";
+import { transactionCategoryStyles } from "@/constants";
 import {
     cn,
     formatAmount,
@@ -15,28 +15,27 @@ import {
     getTransactionStatus,
     removeSpecialCharacters,
 } from "@/lib/utils";
-import { transactionCategoryStyles } from "@/constants";
 
-const CategoryBadge = ({category}: CategoryBadgeProps) => {
-
-    const {
-        borderColor,
-        backgroundColor,
-        textColor,
-        chipBackgroundColor
-    } = transactionCategoryStyles[category as keyof typeof transactionCategoryStyles] || transactionCategoryStyles["default"];
+const CategoryBadge = ({ category }: CategoryBadgeProps) => {
+    const { borderColor, backgroundColor, textColor, chipBackgroundColor } =
+        transactionCategoryStyles[
+            category as keyof typeof transactionCategoryStyles
+        ] || transactionCategoryStyles.default;
 
     return (
         <div className={cn("category-badge", borderColor, chipBackgroundColor)}>
-            <div className={cn("size-2 rounded-full", backgroundColor)}/>
-            <p className={cn("text-[12px] font-medium", textColor)}>{category}</p>
+            <div className={cn("size-2 rounded-full", backgroundColor)} />
+            <p className={cn("text-[12px] font-medium", textColor)}>
+                {category}
+            </p>
         </div>
-    )
-}
+    );
+};
 
-const TransactionTable = ({ transactions }: TransactionTableProps) => {
+const TransactionsTable = ({ transactions }: TransactionTableProps) => {
     return (
-        <Table>
+        <div className="overflow-x-auto">
+            <Table>
             <TableHeader className="bg-[#f9fafb]">
                 <TableRow>
                     <TableHead className="px-2">Transaction</TableHead>
@@ -52,7 +51,7 @@ const TransactionTable = ({ transactions }: TransactionTableProps) => {
                 </TableRow>
             </TableHeader>
             <TableBody>
-                {transactions?.map((t: Transaction) => {
+                {transactions.map((t: Transaction) => {
                     const status = getTransactionStatus(new Date(t.date));
                     const amount = formatAmount(t.amount);
 
@@ -65,44 +64,53 @@ const TransactionTable = ({ transactions }: TransactionTableProps) => {
                             className={`${
                                 isDebit || amount[0] === "-"
                                     ? "bg-[#FFFBFA]"
-                                    : "bg-[F6FEF9]"
-                            } over:bg-none !border-b-DEFAULT`}
+                                    : "bg-[#F6FEF9]"
+                            } !over:bg-none !border-b-DEFAULT`}
                         >
                             <TableCell className="max-w-[250px] pl-2 pr-10">
                                 <div className="flex items-center gap-3">
-                                    <h1 className="text-14 truncate font-semibold text-[#344054]">{removeSpecialCharacters(t.name)}</h1>
+                                    <h1 className="text-14 truncate font-semibold text-[#344054]">
+                                        {removeSpecialCharacters(t.name)}
+                                    </h1>
                                 </div>
                             </TableCell>
 
-                            <TableCell className={`pl-2 pr-10 font-semibold ${
-                                isDebit || amount[0] === "-"
-                                    ? "text-[#f04438]"
-                                    : "text-[#039855]"
-                            }`}>
+                            <TableCell
+                                className={`pl-2 pr-10 font-semibold ${
+                                    isDebit || amount[0] === "-"
+                                        ? "text-[#f04438]"
+                                        : "text-[#039855]"
+                                }`}
+                            >
                                 {isDebit
                                     ? `-${amount}`
                                     : isCredit
-                                    ? `+${amount}`
+                                    ? amount
                                     : amount}
                             </TableCell>
 
                             <TableCell className="pl-2 pr-10">
-                                <CategoryBadge category={status}/>
+                                <CategoryBadge category={status} />
                             </TableCell>
 
-                            <TableCell className="pl-2 pr-10 min-w-32">
+                            <TableCell className="min-w-32 pl-2 pr-10">
                                 {formatDateTime(new Date(t.date)).dateTime}
                             </TableCell>
 
-                            <TableCell className="pl-2 pr-10 capitalize min-w-24">{t.paymentChannel}</TableCell>
+                            <TableCell className="pl-2 pr-10 capitalize min-w-24">
+                                {t.paymentChannel}
+                            </TableCell>
 
-                            <TableCell className="pl-2 pr-10 max-md:hidden"><CategoryBadge category={t.category}/></TableCell>
+                            <TableCell className="pl-2 pr-10 max-md:hidden">
+                                <CategoryBadge category={t.category} />
+                            </TableCell>
                         </TableRow>
                     );
                 })}
             </TableBody>
         </Table>
+        </div>
     );
 };
 
-export default TransactionTable;
+export default TransactionsTable;
